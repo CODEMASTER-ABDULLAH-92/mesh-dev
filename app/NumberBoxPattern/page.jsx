f717a0398575cdb6c1ac50266260e8934416214b
CODEMASTER-ABDULLAH-92 <abdullahsabir9245@gmail.com>
2025-04-21 17:58:09 +0500

    Removing the comments from here

--- app/NumberBoxPattern/page.jsx
+++ app/NumberBoxPattern/page.jsx
@@ -4,31 +4,31 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <Code text={`// #include<iostream>
-// using namespace std;
-// int main(){
-//  for(int row = 1; row <=7; row++){
-//     for(int col = 1; col<=7; col++){
-//         if (row == 1 || row == 7 || col == 1  ||  col == 7)
-//         {
-//             cout<<"4 ";
-//         }
-//         else if(row == 2 || row == 6 || col == 2  ||  col == 6)
-//         {
-//             cout<<"3 ";
-//         }
-//         else if(row == 3 || row == 5 || col == 3  ||  col == 5)
-//         {
-//             cout<<"2 ";
-//         }
-//         else{
-//             cout<<"1 ";
-//         }
-//     }
-//     cout<<endl;
-//  }   
-// return 0;
-// }`} heading={`Number Box Pattern`}/>
+      <Code text={`#include<iostream>
+using namespace std;
+int main(){
+ for(int row = 1; row <=7; row++){
+    for(int col = 1; col<=7; col++){
+        if (row == 1 || row == 7 || col == 1  ||  col == 7)
+        {
+            cout<<"4 ";
+        }
+        else if(row == 2 || row == 6 || col == 2  ||  col == 6)
+        {
+            cout<<"3 ";
+        }
+        else if(row == 3 || row == 5 || col == 3  ||  col == 5)
+        {
+            cout<<"2 ";
+        }
+        else{
+            cout<<"1 ";
+        }
+    }
+    cout<<endl;
+ }   
+return 0;
+}`} heading={`Number Box Pattern`}/>
     </div>
   )
 }
